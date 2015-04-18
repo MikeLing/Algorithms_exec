7acc59a99402fa78fffe0256a83e950046432b1a
Gates_ice <gates@gatesice.com>
2015-04-18 15:47:20 -0500

    Revamped heap sort

--- heap_sort/heap_sort_test.py
+++ heap_sort/heap_sort_test.py
@@ -1,16 +1,20 @@
 import unittest
 from heap_sort import *
+
+
 class Testheapsort(unittest.TestCase):
-	def setUp(self):
-		self.ex = [4,1,3,2,16,9,10,14,8,7]
-		build_max_heap(self.ex)
-		self.ex1 = [4,1,3,2,16,9,10,14,8,7]
-		heap_sort(self.ex1)
-	def test_build_max_heap(self):
-		self.assertEqual(self.ex,[16,14,10,8,7,9,3,2,4,1])
-	def test_heap_sort(self):
-		self.assertEqual(self.ex1,[1,2,3,4,7,8,9,10,14,16])
+
+    def setUp(self):
+        self.ex = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+
+    def test_build_max_heap(self):
+        heap = build_max_heap(self.ex)
+        self.assertEqual(heap, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
+
+    def test_heap_sort(self):
+        heap = heap_sort(self.ex)
+        self.assertEqual(heap, [1, 2, 3, 4, 7, 8, 9, 10, 14, 16])
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
