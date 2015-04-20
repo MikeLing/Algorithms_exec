ca56caaf12436de63b37de253e63939214ab1f43
Gates_ice <gates@gatesice.com>
2015-04-19 21:37:29 -0500

    Quick Sort

--- quick_sort/quick_sort_test.py
+++ quick_sort/quick_sort_test.py
@@ -5,11 +5,11 @@ from quick_sort import *
 class Testheapsort(unittest.TestCase):
 
     def setUp(self):
-        self.ex = [2,8,7,1,3,5,6,4]
+        self.ex = [2, 8, 7, 1, 3, 5, 6, 4]
 
     def test_quicksort(self):
-        array = quicksort(self.ex,1,len(self.ex))
-        self.assertEqual(array, [1,2,3,4,5,6,7,8])
+        quick_sort(self.ex)
+        self.assertEqual(self.ex,  [1, 2, 3, 4, 5, 6, 7, 8])
 
 
 if __name__ == '__main__':
